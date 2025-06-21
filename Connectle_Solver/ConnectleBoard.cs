d5014b8b20ecfc91914deffa284b7b57752e0fdc
NoahSimcox <nssimcox@gmail.com>
2025-06-20 23:05:39 -0400

    Initial commit

---
+++ Connectle_Solver/ConnectleBoard.cs
@@ -0,0 +1,24 @@
+﻿namespace Connectle_Solver
+{
+    public class ConnectleBoard
+    {
+        private int boardSize;
+
+        public ConnectleBoard(int boardSize)
+        {
+            this.boardSize = boardSize;
+        }
+
+        public void drawBoard()
+        {
+            for (int i = 0; i < boardSize; i++)
+            {
+                Console.Write("\n");
+                for (int j = 0; j < boardSize; j++)
+                {
+                    Console.Write("*  ");
+                }
+            }
+        }
+    }
+}
---
+++ Connectle_Solver/Runner.cs
@@ -0,0 +1,10 @@
+﻿namespace Connectle_Solver;
+
+public class Runner
+{
+    static void Main()
+    {
+        ConnectleBoard board = new ConnectleBoard(10);
+        board.drawBoard();
+    }
+}
